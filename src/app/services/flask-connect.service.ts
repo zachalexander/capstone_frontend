24548568b6199839c1ce77ed156ecbbbd556984e
zachalexander <alexander.d.zachary@gmail.com>
2021-05-06 13:37:53 -0400

    fixes services for deploy

--- src/app/services/flask-connect.service.ts
+++ src/app/services/flask-connect.service.ts
@@ -21,22 +21,22 @@ export class FlaskConnectService {
 
 
   getValues(): Observable <any> {
-      return this.http.get('http://localhost:5000/').pipe(map((response => response[0])));
+      return this.http.get('https://cuny-capstone-backend.herokuapp.com/').pipe(map((response => response[0])));
   }
 
   postValues(value): Observable <any> {
-    return this.http.post('http://localhost:5000/posts', value).pipe(map(response => response))
+    return this.http.post('https://cuny-capstone-backend.herokuapp.com/posts', value).pipe(map(response => response))
   }
 
   postCoords(coords): Observable <any> {
-    return this.http.post('http://localhost:5000/coords', coords).pipe(map(response => response))
+    return this.http.post('https://cuny-capstone-backend.herokuapp.com/coords', coords).pipe(map(response => response))
   }
 
   getScrapedData(address): Observable <any> {
-    return this.http.get(`http://localhost:5000/scraped-data/${address}`).pipe(map(response => response))
+    return this.http.get(`https://cuny-capstone-backend.herokuapp.com/scraped-data/${address}`).pipe(map(response => response))
   }
 
   runModel(address): Observable <any> {
-    return this.http.get(`http://localhost:5000/calc/${address}`).pipe(map(response => response))
+    return this.http.get(`https://cuny-capstone-backend.herokuapp.com/calc/${address}`).pipe(map(response => response))
   }
 }
